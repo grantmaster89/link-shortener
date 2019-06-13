7a1b4594008f735289acf265726b3215046cc107
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2019-06-13 07:22:09 -0700

    accidentally deleted the l

--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,7 +1,7 @@
 class ShortLink < ApplicationRecord
   before_save :update_slug
   validates_presence_of :original_url
-  validates_uniqueness_of :original_ur
+  validates_uniqueness_of :original_url
 
   def to_param
     slug
@@ -11,6 +11,10 @@ class ShortLink < ApplicationRecord
 
   def update_slug
     self.slug = generate_short_url_slug
+  end
+
+  def increase_visit
+    self.view_count += 1
   end
 
   def generate_short_url_slug
